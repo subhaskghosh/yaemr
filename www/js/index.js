f5a1d67895dbc30289e39e47330dffe99f2bcfd9
subhas k ghosh <subhas.k.ghosh@gmail.com>
2017-07-09 19:36:47 +0530

    ui-json-realistic

--- www/js/index.js
+++ www/js/index.js
@@ -9,11 +9,24 @@ function loadDefault() {
     request.onreadystatechange = function() {//Call a function when the state changes.
         if (request.readyState == 4) {
             if (request.status == 200 || request.status == 0) {
+		$("#PAGE").empty();
                 var res = JSON.parse(request.responseText);
-		console.log("YAEMR: " + JSON.stringify(res));
+		for (i in res){
+			console.log("YAEMR: " + JSON.stringify(res[i]));
+			var name = res[i]["section_name"];
+			var data = res[i]["data"];
+			$("#PAGE").append("<ons-card><div class=\"title\">" + name + "</div><div class=\"content\"><div id=\"" + name + "\"></div><div style=\"text-align: left; margin-top: 30px;\" id=\"" + name + "_type_area\"></div></div></ons-card>");
+			for(j in data){
+				var symptom = data[j]["name"]
+				$("#" + name).append("<ons-button modifier=\"quiet\">" + symptom + "</ons-button>");
+			}
+			$("#" + name + "_type_area").append("<p><ons-input id=\"type_\"" + name +  "modifier=\"underbar\" placeholder=\"Type your\" float></ons-input></p>")
+		}
                 }
             }
         }
     request.send();
 }
+
+
 
