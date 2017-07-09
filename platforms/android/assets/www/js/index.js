3c7bbb17f78f5b57fa79de3111a122cb03966309
subhas k ghosh <subhas.k.ghosh@gmail.com>
2017-07-09 19:54:11 +0530

    ui-json-realistic

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -15,7 +15,7 @@ function loadDefault() {
 			console.log("YAEMR: " + JSON.stringify(res[i]));
 			var name = res[i]["section_name"];
 			var data = res[i]["data"];
-			$("#PAGE").append("<ons-card><div class=\"title\">" + name + "</div><div class=\"content\"><div id=\"" + name + "\"></div><div style=\"text-align: left; margin-top: 10px;\" id=\"" + name + "_type_area\"></div></div></ons-card>");
+			$("#PAGE").append("<ons-card><div class=\"title\">" + name + "</div><div class=\"content\"><div id=\"" + name + "\"></div><div style=\"text-align: left; margin-top: 30px;\" id=\"" + name + "_type_area\"></div></div></ons-card>");
 			for(j in data){
 				var symptom = data[j]["name"]
 				$("#" + name).append("<ons-button modifier=\"quiet\">" + symptom + "</ons-button>");
